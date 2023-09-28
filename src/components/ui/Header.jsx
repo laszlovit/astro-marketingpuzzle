d15ec8985a9cac3a8dbcd2216b13bccfb8ddfec9
laszlovit <laszlo.vitkai@gmail.com>
2023-09-28 10:54:37 +0200

    update hero section

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -50,7 +50,7 @@ export default function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="custom-shadow bg-white">
+    <header className="custom-shadow bg-white ">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
